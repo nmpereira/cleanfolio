d3677d1fb57a2d7f2c30dc0509607d189fc6f921
nmpereira <nikhilpereira25@gmail.com>
2022-03-07 23:10:56 -0500

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,24 +3,22 @@
 
 const Form = () => (
   <section className='section contact center' id='form'>
-    <div>
-      <form name='cleanfolio' method='POST' data-netlify='true'>
-        <input type='hidden' name='form-name' value='cleanfolio' />
-        <p>
-          <input type='text' name='name' placeholder='Name' />
-        </p>
-        <p>
-          <input type='email' name='email' placeholder='email' />
-        </p>
+    <form name='cleanfolio' method='POST' type='hidden' data-netlify='true'>
+      <input type='hidden' name='form-name' value='cleanfolio' />
+      <p>
+        <input type='text' name='name' placeholder='Name' />
+      </p>
+      <p>
+        <input type='email' name='email' placeholder='email' />
+      </p>
 
-        <p>
-          <textarea name='message' placeholder='Message' />
-        </p>
-        <p>
-          <button type='submit'>Send</button>
-        </p>
-      </form>
-    </div>
+      <p>
+        <textarea name='message' placeholder='Message' />
+      </p>
+      <p>
+        <button type='submit'>Send</button>
+      </p>
+    </form>
   </section>
 )
 
