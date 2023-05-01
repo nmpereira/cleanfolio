f29ca62d5d06d7db6bfcf264a26686ebf7773505
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-05-01 04:17:34 -0400

    Cv page (#4)
    * Update Cv.js
    
    * Create _redirects
    
    * update resume and fix cv page

--- src/components/Cv/Cv.js
+++ src/components/Cv/Cv.js
@@ -1,9 +1,7 @@
 function Cv() {
-  console.log('CV page')
-  // remove /cv from url
   const domain = window.location.href.replace(/\/cv$/, '')
-  console.log({ domain })
   window.location.href = `${domain}/nmpereira_cv.pdf`
+
   return null
 }
 
